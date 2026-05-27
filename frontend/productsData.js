ffb2d19ab8e11ad481d4302cd5a2036d948f5441
hanino-car <haninbdrddin112113@gmail.com>
2026-05-27 23:27:42 +0300

    Added products data + other changes
    removed unwanted default files by reactjs, and added the products data

---
+++ frontend/productsData.js
@@ -0,0 +1,151 @@
+export const productsData = [
+    {
+        productId : "0" ,
+        name : "Dali Melted Clock" ,
+        color : {
+            main : "black" ,
+            variants : [
+                "#F2F2F2" , "#000000"
+            ]
+        },
+        size : {
+            length: 18.1,
+            width: 11.8,
+            height: 13.2
+        },
+        materials: ["Plastic", "Aluminum" , "Copper"],
+        price : 20 ,
+        isOnSale : true ,
+        saleAmount : 20 , 
+        availabilityStatus : "available" , 
+        isBestSeller : true , 
+        description: ["Fully functional wall clock" , "requires 1 AA battery" , "Silent quartz movement" , "Accurate timekeeping"],
+        productImages : [
+            {"url": "https://i.postimg.cc/1XHmK5Wm/black-clock.png", "alt": "main image" },
+        ] ,
+        productVideos : [
+            {},
+            {}
+        ]
+    },
+
+    {
+        productId : "1" ,
+        name : "Sunset Lamp" ,
+        color : {
+            main : "black" ,
+            variants : [
+                "#FF3B30","#FF7A00" , "#FFD400" , "#FF00A8" , "#8A2BE2" , "#2979FF" , "#00D4FF" , "#39FF14" , "#7DFFA8" , "#F5F5F5"
+            ]
+        },
+        size : {
+            length: 18.1,
+            width: 11.8,
+            height: 13.2
+        },
+        materials: ["plastic", "glass", "metal", "paper"],
+        price : 20 ,
+        isOnSale : false ,
+        saleAmount : 0 , 
+        availabilityStatus : "coming_soon" , 
+        isBestSeller : false , 
+        description: ["Warm ambient lighting" , "Multiple color modes" , "180° rotatable lamp head" , "USB powered" , "Includes remote control"],
+        productImages : [
+            {"url": "https://i.postimg.cc/TYfKvqsM/sunset-Lam-main1.png", "alt": "main image"},
+        ] ,
+        productVideos : [
+            {},
+            {}
+        ]
+    },
+
+    {
+    productId : "2" ,
+    name : "Dali Melted Clock" ,
+    color : {
+        main : "silver" ,
+        variants : [
+            "#B5B4B5", "#DDDDDB" , "#000000"
+        ]
+    },
+    size : {
+        length: 18.1,
+        width: 11.8,
+        height: 13.2
+    },
+    materials: ["Plastic", "Aluminum" , "Copper"],
+    price : 20 ,
+    isOnSale : true ,
+    saleAmount : 20 , 
+    availabilityStatus : "available" , 
+    isBestSeller : true , 
+    description: ["Fully functional wall clock" , "requires 1 AA battery" , "Silent quartz movement" , "Accurate timekeeping"],
+    productImages : [
+        {"url": "https://i.postimg.cc/wMHxJsQ4/silver-clock.png", "alt": "main image"},
+    ] ,
+    productVideos : [
+        {},
+        {}
+    ]
+    },
+
+    {
+    productId : "3" ,
+    name : "Dali Melted Clock" ,
+    color : {
+        main : "white" ,
+        variants : [
+            "#F2F2F2","#000000"
+        ]
+    },
+    size : {
+        length: 18.1,
+        width: 11.8,
+        height: 13.2
+    },
+    materials: ["Plastic", "Aluminum" , "Copper"],
+    price : 20 ,
+    isOnSale : false ,
+    saleAmount : 0 , 
+    availabilityStatus : "sold_out" , 
+    isBestSeller : false , 
+    description: ["Fully functional wall clock" , "requires 1 AA battery" , "Silent quartz movement" , "Accurate timekeeping"],
+    productImages : [
+        {"url": "https://i.postimg.cc/sDQv8XYg/white-2.png", "alt": "main image"},
+    ] ,
+    productVideos : [
+        {},
+        {}
+    ]
+    },
+
+    {
+    productId : "4" ,
+    name : "Dali Melted Clock" ,
+    color : {
+        main : "gold" ,
+        variants : [
+            "#C7BC81", "#DDDDDB" , "#000000"
+        ]
+    },
+    size : {
+        length: 18.1,
+        width: 11.8,
+        height: 13.2
+    },
+    materials: ["Plastic", "Aluminum" , "Copper"],
+    price : 20 ,
+    isOnSale : false ,
+    saleAmount : 0 , 
+    availabilityStatus : "available" , 
+    isBestSeller : true , 
+    description: ["Fully functional wall clock" , "requires 1 AA battery" , "Silent quartz movement" , "Accurate timekeeping"],
+    productImages : [
+        {"url": "https://i.postimg.cc/kMcrx3B8/gold-1.png", "alt": "main image"},
+    ] ,
+    productVideos : [
+        {},
+        {}
+    ]
+    },
+]
